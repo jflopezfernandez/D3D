91fad554d2ed14f720f93a155513b06ff048222f
lopezfjose <jflopezfernandez@gmail.com>
2018-05-02 21:59:19 -0400

    Started a concept for a list data structure using std::vector<std::string>

--- d3d/main.cpp
+++ d3d/main.cpp
@@ -4,14 +4,51 @@
 #include <Windows.h>
 #endif // WINDOWS_HEADER_INCLUDED
 
+#ifndef STDLIB_STRING_INCLUDED
+#define STDLIB_STRING_INCLUDED
+#include <string>
+#endif // STDLIB_STRING_INCLUDED
+
+#ifndef STDLIB_VECTOR_INCLUDED
+#define STDLIB_VECTOR_INCLUDED
+#include <vector>
+#endif // STDLIB_VECTOR_INCLUDED
+
 #ifndef D3D_IO_HPP
 #include "IO.hpp"
 #endif // D3D_IO_HPP
 
+using List = std::vector<std::string>;
 
-int main()
+template <typename T>
+void PrintVector(const std::vector<T>& vector) {
+	for (auto& v : vector) {
+		std::cout << v << '\n';
+	}
+}
+
+void PrintList(const std::vector<std::string>& vector) {
+	PrintVector(vector);
+}
+
+int main(int argc, char *argv[])
 {
-	IO::PrintLn("testing...");
+	List people = {
+		"Jose",
+		"Jake",
+		"John",
+		"Jeff",
+		"Julio",
+		"Juan"
+	};
 
-	system("PAUSE");
+	PrintList(people);
+
+	/*std::vector<std::string> applicationArgs;
+
+	for (auto i = 1; i < argc; i++) {
+		applicationArgs.emplace_back(std::string(argv[i]));
+	}
+
+	PrintVector<std::string>(applicationArgs);*/
 }
